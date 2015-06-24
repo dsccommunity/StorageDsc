9efc745aaf98b3c60f4fff5250d3340a31959708
Alexei Andreyev <alexei.andreyev@gmail.com>
2015-06-24 16:29:16 +0100

    Added support for:  - Drive Letter  - Size  - FSLabel

--- DSCResources/MSFT_xDisk/MSFT_xDisk.psm1
+++ DSCResources/MSFT_xDisk/MSFT_xDisk.psm1
@@ -9,14 +9,23 @@ function Get-TargetResource
     (
         [parameter(Mandatory)]
         [uint32] $DiskNumber,
-
-        [string] $DriveLetter
+        [string] $DriveLetter,
+        [UInt64] $Size,
+        [string] $FSLabel
     )
 
-    $disk = Get-Disk -Number $DiskNumber
+    $Disk = Get-Disk -Number $DiskNumber -ErrorAction SilentlyContinue
+    
+    $Partition = Get-Partition -DriveLetter $DriveLetter -ErrorAction SilentlyContinue
+
+    $FSLabel = Get-Volume -DriveLetter $DriveLetter -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FileSystemLabel
+
+
     $returnValue = @{
-        DiskNumber = $disk.Number
-        DriveLetter = $disk | Get-Partition | Where-Object { $_.DriveLetter -ne "`0" } | Select-Object -ExpandProperty DriveLetter
+        DiskNumber = $Disk.Number
+        DriveLetter = $Partition.DriveLetter
+        Size = $Partition.Size
+        FSLabel = $FSLabel
     }
     $returnValue
 }
@@ -27,53 +36,84 @@ function Set-TargetResource
     (
         [parameter(Mandatory)]
         [uint32] $DiskNumber,
-
-        [string] $DriveLetter
+        [string] $DriveLetter,
+        [UInt64] $Size,
+        [string] $FSLabel
     )
     
-    $disk = Get-Disk -Number $DiskNumber
-    
-    if ($disk.IsOffline -eq $true)
+    try
     {
-        Write-Verbose 'Setting disk Online'
-        $disk | Set-Disk -IsOffline $false
-    }
+        $Disk = Get-Disk -Number $DiskNumber -ErrorAction Stop
     
-    if ($disk.IsReadOnly -eq $true)
-    {
-        Write-Verbose 'Setting disk to not ReadOnly'
-        $disk | Set-Disk -IsReadOnly $false
-    }
-    
-    if ($disk.PartitionStyle -eq "RAW")
-    {
-        Write-Verbose -Message "Initializing disk number '$($DiskNumber)'..."
-
-        $disk | Initialize-Disk -PartitionStyle GPT -PassThru
-        if ($DriveLetter)
+        if ($Disk.IsOffline -eq $true)
         {
-            $partition = $disk | New-Partition -DriveLetter $DriveLetter -UseMaximumSize
+            Write-Verbose 'Setting disk Online'
+            $Disk | Set-Disk -IsOffline $false
+        }
+        
+        if ($Disk.IsReadOnly -eq $true)
+        {
+            Write-Verbose 'Setting disk to not ReadOnly'
+            $Disk | Set-Disk -IsReadOnly $false
+        }
+
+        Write-Verbose -Message "Checking existing disk partition style..."
+        if (($Disk.PartitionStyle -ne "GPT") -and ($Disk.PartitionStyle -ne "RAW"))
+        {
+            Throw "Disk '$($DiskNumber)' is already initialised with '$($Disk.PartitionStyle)'"
         }
         else
         {
-            $partition = $disk | New-Partition -AssignDriveLetter -UseMaximumSize
+            if ($Disk.PartitionStyle -eq "RAW")
+            {
+                Write-Verbose -Message "Initializing disk number '$($DiskNumber)'..."
+                $Disk | Initialize-Disk -PartitionStyle "GPT" -PassThru
+            }
+            else
+            {
+                Write-Verbose -Message "Disk number '$($DiskNumber)' is already configured for 'GPT'"
+            }
+        }
+
+        Write-Verbose -Message "Creating the partition..."
+        if ($DriveLetter -and $Size )
+        {
+            $Partition = $Disk | New-Partition -DriveLetter $DriveLetter -Size $Size
+        }
+        elseif ($DriveLetter -and (-not $Size))
+        {
+            $Partition = $Disk | New-Partition -DriveLetter $DriveLetter -UseMaximumSize
+        }
+        elseif ($Size -and (-not $DriveLetter))
+        {
+            $Partition = $Disk | New-Partition -AssignDriveLetter -Size $Size
+        }
+        else
+        {
+            $Partition = $Disk | New-Partition -AssignDriveLetter -UseMaximumSize
         }
 
         # Sometimes the disk will still be read-only after the call to New-Partition returns.
         Start-Sleep -Seconds 5
 
-        $volume = $partition | Format-Volume -FileSystem NTFS -Confirm:$false
+        Write-Verbose -Message "Formatting the volume..."
+        if ($FSLabel)
+        {
+            $Volume = $Partition | Format-Volume -FileSystem NTFS -NewFileSystemLabel $FSLabel -Confirm:$false 
+        }
+        else
+        {
+            $Volume = $Partition | Format-Volume -FileSystem NTFS -Confirm:$false
+        }
 
         Write-Verbose -Message "Successfully initialized disk number '$($DiskNumber)'."
+        
+        $global:DSCMachineStatus = 1
     }
-    
-    if (($disk | Get-Partition | Where-Object { $_.DriveLetter -ne "`0" } | Select-Object -ExpandProperty DriveLetter) -ne $DriveLetter)
+    catch
     {
-        Write-Verbose "Changing drive letter to $DriveLetter"
-        Set-Partition -DiskNumber $disknumber -PartitionNumber (Get-Partition -Disk $disk | Where-Object { $_.DriveLetter -ne "`0" } | Select-Object -ExpandProperty PartitionNumber) -NewDriveLetter $driveletter
+        Throw "Disk Set-TargetResource failed with the following error: '$($Error[0])'"
     }
-    
-    $global:DSCMachineStatus = 1    
 }
 
 function Test-TargetResource
@@ -83,39 +123,62 @@ function Test-TargetResource
     (
         [parameter(Mandatory)]
         [uint32] $DiskNumber,
-
-        [string] $DriveLetter
+        [string] $DriveLetter,
+        [UInt64] $Size,
+        [string] $FSLabel
     )
 
     Write-Verbose -Message "Checking if disk number '$($DiskNumber)' is initialized..."
-    $disk = Get-Disk -Number $DiskNumber
-    if (-not $disk)
-    {
-        throw "Disk number '$($DiskNumber)' does not exist."
-    }
-    if ($disk.PartitionStyle -ne "RAW")
-    {
-        Write-Verbose "Disk number '$($DiskNumber)' has already been initialized."
+    $Disk = Get-Disk -Number $DiskNumber -ErrorAction SilentlyContinue
 
-        $driveLetterFromDisk = $disk | Get-Partition | Where-Object { $_.DriveLetter -ne "`0" } | Select-Object -ExpandProperty DriveLetter
-        if ($DriveLetter -ne "" -and $DriveLetter -ne $driveLetterFromDisk)
-        {
-            write-verbose "Disk number '$($DiskNumber)' has an unexpected drive letter. Expected: $DriveLetter. Actual: $driveLetterFromDisk."
-            return $false
-        }
-        if ($disk.IsOffline -eq $true) 
-        {
-            write-verbose "Disk is set Offline."
-            return $false
-        }
-        if ($disk.IsReadOnly -eq $true) 
-        {
-            write-verbose "Disk is set ReadOnly."
-            return $false
-        }
-        return $true
+    if (-not $Disk)
+    {
+        Write-Error "Disk number '$($DiskNumber)' does not exist."
+        return $false
     }
-    return $false
+
+    if ($Disk.IsOffline -eq $true)
+    {
+        Write-Error 'Disk is not Online'
+        return $false
+    }
+    
+    if ($Disk.IsReadOnly -eq $true)
+    {
+        Write-Error 'Disk set as ReadOnly'
+        return $false
+    }
+
+    if ($Disk.PartitionStyle -ne "GPT")
+    {
+        Write-Error "Disk '$($DiskNumber)' is initialised with '$($Disk.PartitionStyle)' partition style"
+        return $false
+    }
+
+    # DriveLetter
+    $Partition = Get-Partition -DriveLetter $DriveLetter -ErrorAction SilentlyContinue
+    if ( -not $Partition)
+    {
+        Write-Error "Drive $DriveLetter was not found"
+        return $false
+    }
+
+    # Drive size
+    if ($Partition.Size -ne $Size)
+    {
+        Write-Error "Drive $DriveLetter size does not match defined value"
+        return $false
+    }
+
+    # Volume label
+    $Label = Get-Volume -DriveLetter $DriveLetter -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FileSystemLabel
+    if ($FSLabel -ne $Label)
+    {
+        Write-Error "Volume $DriveLetter label does not match defined value"
+        return $false
+    }
+
+    return $true
 }
 
 
