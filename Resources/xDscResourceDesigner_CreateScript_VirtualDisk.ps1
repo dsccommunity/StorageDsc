5923d2abba73b93d5e1422e50ea2905d41e787f9
peppekerstens <peppe.kerstens@iton.nl>
2016-10-18 14:25:33 +0200

    nearly finished developing VirtualDisk resource

--- Resources/xDscResourceDesigner_CreateScript_VirtualDisk.ps1
+++ Resources/xDscResourceDesigner_CreateScript_VirtualDisk.ps1
@@ -1,6 +1,6 @@
 $modules = 'C:\Program Files\WindowsPowerShell\Modules\'
 $modulename = 'xStorage'
-$Description = 'This module is used to create a VirtualDisk on an existing StoragePool. Functionality is limited to create, add a disk to existing, a rename of existing and COMPLETE destroy. It can only filter on disk size and/or numberofdisks'
+$Description = 'This module is used to create a VirtualDisk on an existing StoragePool. Functionality is limited to create and COMPLETE destroy.'
 
 if (!(test-path (join-path $modules $modulename))) {
 
@@ -12,10 +12,11 @@ if (!(test-path (join-path $modules $modulename))) {
                 Path = $modules
                 }
     $P = @()
-    $P += New-xDscResourceProperty -Name FriendlyName -Type String -Attribute Key -Description 'This setting provides a unique name for the configuration'
-    $P += New-xDscResourceProperty -Name NewFriendlyName -Type String -Attribute Write -Description 'Specifies the new name of an existing StorageSpace configuration'
-    $P += New-xDscResourceProperty -Name NumberOfDisks -Type Uint32 -Attribute Write -Description "Specifies the number of disks to be added, adds any 'canpool' disk"
-    $P += New-xDscResourceProperty -Name DriveSize -Type Uint32 -Attribute Write -Description "Specifies the size of the disk (in GB) to filter on in the 'canpool' collection."
+    $P += New-xDscResourceProperty -Name FriendlyName -Type String -Attribute Key -Description 'Specifies the name of the VirtualDisk te be created'
+    $P += New-xDscResourceProperty -Name StoragePoolFriendlyName -Type String -Attribute Key -Description 'Specifies the StorageSpace on which the VirtualDisk has to be placed'
+    $P += New-xDscResourceProperty -Name Size -Type Uint32 -Attribute Write -Description "Specifies the size of the disk (in GB)"
+    $P += New-xDscResourceProperty -Name ProvisioningType -Type String -Attribute Write -ValidateSet 'Thin','Fixed' -Description "ProvisioningType of the VirtualDisk. If omitted, defaults to Fixed"
+    $P += New-xDscResourceProperty -Name ResiliencySettingName -Type String -Attribute Write -ValidateSet 'Simple','Mirror','Parity' -Description "Resiliency setting of the VirtualDisk. If omitted, defaults to Mirror"
     $P += New-xDscResourceProperty -Name Ensure -Type String -Attribute Write -ValidateSet 'Present','Absent' -Description 'Determines whether the setting should be applied or removed'
     New-xDscResource -Name VirtualDisk -Property $P -FriendlyName VirtualDisk @standard
 }
