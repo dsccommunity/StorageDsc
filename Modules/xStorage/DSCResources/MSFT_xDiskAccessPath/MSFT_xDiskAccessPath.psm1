2ee3aa8e4d8bbd9de463e9f1055317a6309075c1
William <positivism1@gmail.com>
2017-12-11 12:13:51 +1100

    Updates requested

--- Modules/xStorage/DSCResources/MSFT_xDiskAccessPath/MSFT_xDiskAccessPath.psm1
+++ Modules/xStorage/DSCResources/MSFT_xDiskAccessPath/MSFT_xDiskAccessPath.psm1
@@ -60,7 +60,7 @@ function Get-TargetResource
         $DiskId,
 
         [Parameter()]
-        [ValidateSet('Number','UniqueId','Guid')]
+        [ValidateSet('Number','UniqueId')]
         [System.String]
         $DiskIdType = 'Number',
 
@@ -90,10 +90,13 @@ function Get-TargetResource
     # Validate the AccessPath parameter adding a trailing slash
     $AccessPath = Assert-AccessPathValid -AccessPath $AccessPath -Slash
 
-    # Get the Disk using the identifiers supplied
-    $disk = Get-DiskByIdentifier `
-        -DiskId $DiskId `
-        -DiskIdType $DiskIdType
+    $diskIdParameter = @{
+        $DiskIdType = $DiskId
+    }
+
+    $disk = Get-Disk `
+        @diskIdParameter `
+        -ErrorAction SilentlyContinue
 
     # Get the partitions on the disk
     $partition = $disk | Get-Partition -ErrorAction SilentlyContinue
@@ -167,7 +170,7 @@ function Set-TargetResource
         $DiskId,
 
         [Parameter()]
-        [ValidateSet('Number','UniqueId','Guid')]
+        [ValidateSet('Number','UniqueId')]
         [System.String]
         $DiskIdType = 'Number',
 
@@ -188,7 +191,7 @@ function Set-TargetResource
         [System.String]
         $FSFormat = 'NTFS'
     )
-
+    
     Write-Verbose -Message ( @(
             "$($MyInvocation.MyCommand): "
             $($localizedData.SettingDiskMessage -f $DiskIdType,$DiskId,$AccessPath)
@@ -197,10 +200,13 @@ function Set-TargetResource
     # Validate the AccessPath parameter adding a trailing slash
     $AccessPath = Assert-AccessPathValid -AccessPath $AccessPath -Slash
 
-    # Get the Disk using the identifiers supplied
-    $disk = Get-DiskByIdentifier `
-        -DiskId $DiskId `
-        -DiskIdType $DiskIdType
+    $diskIdParameter = @{
+        $DiskIdType = $DiskId
+    }
+
+    $disk = Get-Disk `
+        @diskIdParameter `
+        -ErrorAction Stop
 
     if ($disk.IsOffline)
     {
@@ -507,7 +513,7 @@ function Test-TargetResource
         $DiskId,
 
         [Parameter()]
-        [ValidateSet('Number','UniqueId','Guid')]
+        [ValidateSet('Number','UniqueId')]
         [System.String]
         $DiskIdType = 'Number',
 
@@ -529,27 +535,24 @@ function Test-TargetResource
         $FSFormat = 'NTFS'
     )
 
-  Try 
+    try
     {
-          $AccessPathDisk = $AccessPath.split(":")[0]
-            Write-Verbose -Message ( @(
-            "$($MyInvocation.MyCommand): "
-            $($localizedData.TestingPSDriveMessage -f $AccessPathDisk,$AccessPath)
-        ) -join '' )
-         $TestDrive = Get-PSDrive $AccessPathDisk -ErrorAction Stop
+        $AccessPathDisk = $AccessPath.split(":")[0]
+        Write-Verbose -Message ( @(
+                "$($MyInvocation.MyCommand): "
+                $($localizedData.TestingPSDriveMessage -f $AccessPathDisk,$AccessPath)
+            ) -join '' )
+         $null = Get-PSDrive $AccessPathDisk -ErrorAction Stop
     }
-    Catch
+    catch
     {
-        
-              Write-Verbose -Message ( @(
-            "$($MyInvocation.MyCommand): "
-            $($localizedData.UnavailablePSDriveMessage -f $AccessPathDisk,$AccessPath)
-        ) -join '' )
+        Write-Verbose -Message ( @(
+       "$($MyInvocation.MyCommand): "
+                $($localizedData.UnavailablePSDriveMessage -f $AccessPathDisk,$AccessPath)
+            ) -join '' )
+        $null = Get-PSDrive
+    }
 
-   
-        Get-PSDrive | out-null
-    }
-    
     Write-Verbose -Message ( @(
             "$($MyInvocation.MyCommand): "
             $($localizedData.TestingDiskMessage -f $DiskIdType,$DiskId,$AccessPath)
@@ -563,10 +566,13 @@ function Test-TargetResource
             $($localizedData.CheckDiskInitializedMessage -f $DiskIdType,$DiskId)
         ) -join '' )
 
-    # Get the Disk using the identifiers supplied
-    $disk = Get-DiskByIdentifier `
-        -DiskId $DiskId `
-        -DiskIdType $DiskIdType
+    $diskIdParameter = @{
+        $DiskIdType = $DiskId
+    }
+
+    $disk = Get-Disk `
+        @diskIdParameter `
+        -ErrorAction SilentlyContinue
 
     if (-not $disk)
     {
