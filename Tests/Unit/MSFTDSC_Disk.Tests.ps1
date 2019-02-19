2c1b2dac769420ae6c751a08a63a85fda8197bb5
Josh Swain <jswain@wayfair.com>
2019-02-19 16:40:37 -0500

    splitting unit tests for readonly and writable boolean

--- Tests/Unit/MSFTDSC_Disk.Tests.ps1
+++ Tests/Unit/MSFTDSC_Disk.Tests.ps1
@@ -1346,7 +1346,7 @@ try
                         -ParameterFilter $script:parameterFilter_MockedDisk0Number
                     Assert-MockCalled -CommandName Set-Disk -Exactly -Times 0
                     Assert-MockCalled -CommandName Initialize-Disk -Exactly -Times 0
-                    Assert-MockCalled -CommandName Get-Partition -Exactly -Times 31
+                    Assert-MockCalled -CommandName Get-Partition -Exactly -Times 4
                     Assert-MockCalled -CommandName Get-Volume -Exactly -Times 1
                     Assert-MockCalled -CommandName New-Partition -Exactly -Times 1 `
                         -ParameterFilter {
