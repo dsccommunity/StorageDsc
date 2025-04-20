290921d6681d5d355151a4e9eeb22371403f63eb
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-04-20 17:45:04 +0100

    Revert "Convert built in modules to buildable modules"
    This reverts commit 89d40341f8c4a96582bd29a4b49a41c37ecf22b1.

--- source/Modules/StorageDsc.Common/prefix.ps1
+++
@@ -1,6 +0,0 @@
-$script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath '../DscResource.Common'
-
-Import-Module -Name $script:dscResourceCommonModulePath
-
-# Import Localization Strings
-$script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
